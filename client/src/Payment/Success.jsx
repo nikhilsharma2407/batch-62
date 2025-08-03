b7545cdee88462451e9c613f466a45939f3f2f76
Nikhil Sharma <nikhil.sharma2407@gmail.com>
2025-08-03 23:06:10 +0530

    removed comments

--- client/src/Payment/Success.jsx
+++ client/src/Payment/Success.jsx
@@ -7,18 +7,9 @@ const Success = () => {
     useEffect(() => {
         (async () => {
             const { data } = (await axiosInstance.get(ENDPOINTS.STRIPE.GET_CHECKOUT_SESSION)).data;
-            console.log("🚀 ~ Success ~ data:", data);
             setData(data);
         })()
     }, [])
-    // {
-    //     "stripeSessionId": "cs_test_a16sIVTszDiP0FsVXqY5KpR7YcbN1unlQOdVOKO9xR1ImCDSYAKsHoGB4E",
-    //     "username": "nikhil1234",
-    //     "amountTotal": 9506.28,
-    //     "currency": "inr",
-    //     "paymentStatus": "paid",
-    //     "products": []
-    // }
     return (
         <>
             <div>Payment Successful</div>
